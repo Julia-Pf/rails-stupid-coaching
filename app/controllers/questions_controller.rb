a8b6f7d0a30f9de215fbdb6dacb521c7574c63b9
Julia Pfaffenzeller <julia.pfaffenzeller@web.de>
2024-01-22 14:43:28 +0100

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,8 @@ class QuestionsController < ApplicationController
     @input = params[:input]
     if @input == "I am going to work"
       @answer = "Great"
+    elsif @input.include? "?"
+      @answer = "Silly question, get dressed and go to work!."
     else @answer = "I don't care, get dressed and go to work!"
     end
   end
